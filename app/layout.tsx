270d7af6a99d3e46dc043510bce9740e35f6649b
Nguyễn Minh Thông <mtnchjll@gmail.com>
2025-06-16 13:11:51 +0700

    Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/theme-provider/ThemeProvider";
 import { CodeXmlIcon } from "lucide-react";
 import Navigation from "@/components/Navigation";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-nunito",
+});
 
 export const metadata: Metadata = {
   title: "Weather Forecast",
@@ -19,14 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}, min-h-screen `}>
+      <body className={`${nunito.className}, min-h-screen `}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <div className="container mx-auto flex flex-col px-[1rem] antialiased md:px-[2rem] ">
+          <div className="container mx-auto flex flex-col gap-2 py-2 px-[1rem] antialiased md:px-[2rem] ">
             <Navigation />
             {children}
             <footer
